12ed6c49856a7e785c6c3ac2e466c1b48867d46b
idbord <idbord@qq.com>
2016-04-01 23:12:53 +0800

    v1.0

--- config_example.py
+++ config_example.py
@@ -4,23 +4,24 @@ __author__ = 'idbord'
 # 数据库备份配置
 db_backup = True
 databases = [
-    "hilaw",
-    "ebook"
+    "",
+    ""
 ]
 
 # 文件备份配置
 file_backup = True
 file_paths = [
-    "/home/idbord/Pictures"
+    "",
+    ""
 ]
 
 # 七牛上传配置
 # 需要填写你的 Access Key 和 Secret Key
-access_key = 'l__D-PP23Eyd-_L9F83_iURF4gp7-tqkeJCYXWZG'
-secret_key = 'FsdLbm7lEdLhg7DS4QX07oyvhP4h9_1ohJumdCm5'
+access_key = ''
+secret_key = ''
 
 # 要上传的空间
-bucket_name = 'fxy-backup'
+bucket_name = ''
 
 # 设置备份周期,秒
 backup_circle = 86400
